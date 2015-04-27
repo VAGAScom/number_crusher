b3b884fba910d6e76188ec1fee4ec472df48d7cd
Ronie Uliana <ronie.uliana@gmail.com>
2015-04-27 16:27:12 -0300

    Better specs

--- spec/lib/number_crusher/resampling_with_replacement_spec.rb
+++ spec/lib/number_crusher/resampling_with_replacement_spec.rb
@@ -1,6 +1,34 @@
 require "spec_helper"
 
 describe ResamplingWithReplacement do
+  RSpec::Matchers.define_negated_matcher :an_array_excluding, :include
+  matcher :have_size do |expect_size|
+    match do |actual|
+      actual.size == expect_size
+    end
+  end
+
+  matcher :average_of_sums_between do |lower, upper|
+    match do |actual|
+      average_of_sums = actual.
+        map { |v| v.reduce(0) { |a, e| a + e } }.
+        reduce(0) { |a, e| a + e } / actual.size.to_f
+      (lower..upper).include?(average_of_sums)
+    end
+  end
+
+  matcher :be_randomized do |expect|
+    match do |actual|
+      expect(actual).to \
+        all(have_size(3)) &
+        include([1, 2, 3]) &
+        include(an_array_excluding(3)) &
+        include(an_array_excluding(2)) &
+        include(an_array_excluding(1)) &
+        average_of_sums_between(5.5, 6.5)
+    end
+  end
+
   it "returns empty if zero (or negative) resamplings" do
     expect(ResamplingWithReplacement([1, 2, 3], samples: 0)).to eq []
     expect(ResamplingWithReplacement([1, 2, 3], samples: -1)).to eq []
@@ -14,32 +42,14 @@ describe ResamplingWithReplacement do
 
     it "randomize numbers with replacement" do
       samples = (1..100).map { ResamplingWithReplacement([1, 2, 3]).first }
-      expect(samples.all? { |s| s.size == 3 }).to be_truthy
-      expect(samples.any? { |s| s != [1, 2, 3] }).to be_truthy
-      expect(samples.all? { |s| s.include?(3) }).to be_falsey
-      expect(samples.all? { |s| s.include?(2) }).to be_falsey
-      expect(samples.all? { |s| s.include?(1) }).to be_falsey
-
-      average_of_sums = samples.
-        map { |v| v.reduce(0) { |a, e| a + e } }.
-        reduce(0) { |a, e| a + e } / samples.size.to_f
-      expect(average_of_sums).to be_between(5.5, 6.5).inclusive
+      expect(samples).to be_randomized
     end
   end
 
   context "with more samples" do
     it "randomize numbers with replacement" do
       samples = ResamplingWithReplacement([1, 2, 3], samples: 100)
-      expect(samples.all? { |s| s.size == 3 }).to be_truthy
-      expect(samples.any? { |s| s != [1, 2, 3] }).to be_truthy
-      expect(samples.all? { |s| s.include?(3) }).to be_falsey
-      expect(samples.all? { |s| s.include?(2) }).to be_falsey
-      expect(samples.all? { |s| s.include?(1) }).to be_falsey
-
-      average_of_sums = samples.
-        map { |v| v.reduce(0) { |a, e| a + e } }.
-        reduce(0) { |a, e| a + e } / samples.size.to_f
-      expect(average_of_sums).to be_between(5.5, 6.5).inclusive
+      expect(samples).to be_randomized
     end
   end
 end
